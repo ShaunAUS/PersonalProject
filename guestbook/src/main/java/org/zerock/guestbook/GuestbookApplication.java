ae1c451431a854f1d3dc54cb1332dc269f411a43
ShaunAUS <86937655+ShaunAUS@users.noreply.github.com>
2021-07-17 02:16:33 +0900

    방명록 guestbook
    엔티티,컨트룰러, 설정

---
+++ guestbook/src/main/java/org/zerock/guestbook/GuestbookApplication.java
@@ -0,0 +1,15 @@
+package org.zerock.guestbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing //jpa 리스너 활성화시키기 위함
+public class GuestbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuestbookApplication.class, args);
+	}
+
+}
