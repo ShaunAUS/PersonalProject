cc0cd426c138379a372b077f3fe2c228ecae3fdc
ShaunAUS <86937655+ShaunAUS@users.noreply.github.com>
2021-07-23 00:55:57 +0900

    board
    1:jpql,  로 board, member, reply 엔티티들 묶기,
    2:pk, fk 관계, 설정
    3: 게시판 리스트에  댓글 수

---
+++ board/src/main/java/com/example/board/entity/Reply.java
@@ -0,0 +1,31 @@
+package com.example.board.entity;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+public class Reply extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long rno;
+    private String text;
+    private String replyer;
+
+
+    //jpa에서 관계 고민할떄는 fk쪽을 먼저해석,
+    //이 애노테이션은 데이터베이스상에서 외래키의 관계로 연결된 엔티티 클래스에 설정한다
+    //join= fk쪽의 엔티티가져올떄 pk 쪽엔티티도 가져온다
+    // fetch= 연관관계 데이터 어떻게 가져올까. Eager= 연관관계가 있는 모든 엔티티, lazy= 그반대
+    @ManyToOne
+    private Board board;
+
+
+}
---
+++ board/src/main/java/com/example/board/repository/BoardRepository.java
@@ -0,0 +1,40 @@
+package com.example.board.repository;
+
+import com.example.board.entity.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface BoardRepository extends JpaRepository<Board,Long> {
+
+    @Query("select b,w from Board b left join b.writer w where b.bno=:bno")
+    List<Object[]> getBoardWithWriter(@Param("bno") Long bno);
+
+
+
+    @Query("select b, r FROM Board b LEFT JOIN Reply r ON r.board=b WHERE b.bno=:bno")
+    List<Object[]> getBoardWithReply(@Param("bno") Long bno);
+
+
+
+    @Query(value="SELECT b,w, count(r) "+
+            "FROM Board b"+
+            "LEFT JOIN b.writer w"+
+            "LEFT JOIN Reply r ON r.board=b"+
+            "GROUP BY b",
+            countQuery="SELECT count(b) FROM Board")
+    Page<Object[]> getBoardWithReplyCount(Pageable pagealbe);  //목록 화면에 필요한 필터
+
+
+    @Query(value="SELECT b,w, count(r) "+
+            "FROM Board b"+
+            "LEFT JOIN b.writer w"+
+            "LEFT OUTER JOIN Reply r ON r.board=b"+
+            "WHERE b.bno=:bno")
+          Object getBoardByBno(@Param("bno") Long bno);
+
+}
