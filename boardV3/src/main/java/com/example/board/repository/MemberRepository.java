c7fbdde0d2cd8f032b53d1214378cce231b49a9a
ShaunAUS <86937655+ShaunAUS@users.noreply.github.com>
2021-07-26 01:52:13 +0900

    Revert "boardV3"
    This reverts commit 007688b43c82df172d23d1a4d32eb9a0229eb621.

--- boardV3/src/main/java/com/example/board/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.board.repository;
-
-import com.example.board.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member,String> {
-}
