8f64df08dfe7b79ea5143dbfeae4c2a0ee36b375
ShaunAUS <86937655+ShaunAUS@users.noreply.github.com>
2021-11-15 13:36:30 +0900

    Revert "Todo practice"
    This reverts commit 357b12bc2cc3d60d7fbf65fec9774afde1d1ed2b.

--- Todo/src/main/java/com/example/Todo/repository/ToDoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Todo.repository;
-
-import com.example.Todo.entity.TodoEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ToDoRepository extends JpaRepository<TodoEntity,Long> {
-
-
-}
